9a54570d4c16173fb343eaa2553d46da2531bf84
MansfieldPlumbing <109449446+MansfieldPlumbing@users.noreply.github.com>
2026-09-14 23:12:35 -0400

    Remove .Wip/ from repository

--- .Wip/Persistence/PROFILE.DeviceProof.ps1
+++
@@ -1,6 +0,0 @@
-$view = [Android.Widget.TextView]::new($Activity)
-$view.Text = "PROFILE_OK`nPSScriptRoot=$PSScriptRoot"
-$view.TextSize = 36
-$view.SetTextColor([Android.Graphics.Color]::White)
-$view.SetBackgroundColor([Android.Graphics.Color]::Rgb(11, 61, 46))
-$Activity.SetContentView($view)
